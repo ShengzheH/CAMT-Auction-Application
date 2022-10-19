ed94f39e95a50ab65222268eeec667323f228621
HaolongHuai <1612907625@qq.com>
2022-10-19 23:16:34 +0800

    update class diagram

--- src/main/BasicConfig/Admin.java
+++ src/main/BasicConfig/Admin.java
@@ -1,2 +1,27 @@
-public class Admin {
+package BasicConfig;
+
+public class Admin extends User{
+    User user;
+
+    public Admin(String name, String password, String address, String notel, User user) {
+        super(name, password, address, notel);
+        this.user = user;
+    }
+
+    public Admin(int id, String name, String password, String address, String notel, User user) {
+        super(id, name, password, address, notel);
+        this.user = user;
+    }
+
+    public Admin(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
